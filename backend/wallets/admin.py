0119b258269417af589ae17d384b3c61e7756cc2
maturelion <maturelion1@gmail.com>
2023-05-12 21:44:39 +0100

    wallet admin

--- backend/wallets/admin.py
+++ backend/wallets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from wallets.models import Wallet
 
-# Register your models here.
+class WalletAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ["user", "balance"]
+
+admin.site.register(Wallet, WalletAdmin)
